2277b84a2d7f4312223cf2a896c4d21c973b7c2f
defnot001 <marc.miller994@gmail.com>
2022-12-11 18:11:25 +0100

    feat: assertion for PteroBackupList

--- src/client/util/assertions.ts
+++ src/client/util/assertions.ts
@@ -1,4 +1,7 @@
-import type { PteroBackupCreateResponseData } from '../types/interfaces';
+import type {
+  PteroBackupCreateResponseData,
+  PteroBackupListResponseData,
+} from '../types/interfaces';
 
 export function assertsPteroBackupCreateResponseData(
   data: unknown,
@@ -8,8 +11,24 @@ export function assertsPteroBackupCreateResponseData(
     data !== null &&
     'object' in data &&
     data.object === 'backup'
-  )
+  ) {
     return;
+  }
+
+  throw new Error();
+}
+
+export function assertsPteroBackupListResponseData(
+  data: unknown,
+): asserts data is PteroBackupListResponseData {
+  if (
+    typeof data === 'object' &&
+    data !== null &&
+    'object' in data &&
+    data.object === 'list'
+  ) {
+    return;
+  }
 
   throw new Error();
 }
