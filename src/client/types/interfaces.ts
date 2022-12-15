d765e7942af592addaf5bd3c39a44eeb63125e59
defnot001 <marc.miller994@gmail.com>
2022-12-15 11:11:32 +0100

    removed interfaces in favor of zod type inference

--- src/client/types/interfaces.ts
+++ src/client/types/interfaces.ts
@@ -52,23 +52,6 @@ export interface BackupListResponse {
   meta: BackupMeta;
 }
 
-interface BackupCreateAttributes {
-  uuid: string;
-  is_successful: boolean;
-  is_locked: boolean;
-  name: string;
-  ignored_files: string[];
-  checksum: null;
-  bytes: number;
-  created_at: string;
-  completed_at: string;
-}
-
-export interface BackupCreateResponse {
-  object: 'backup';
-  attributes: BackupCreateAttributes;
-}
-
 export interface APIError {
   code: string;
   status: string;
@@ -93,85 +76,6 @@ export interface AccountDetailsResponse {
 export interface BackupOptions {
   backupName?: string;
   locked?: boolean;
-}
-
-interface ServerAttributeLimits {
-  memory: number;
-  swap: number;
-  disk: number;
-  io: number;
-  cpu: number;
-  threads: number;
-  oom_disabled: boolean;
-}
-
-interface ServerAttributeFeatureLimits {
-  databases: number;
-  allocations: number;
-  backups: number;
-}
-
-interface ServerAttributeRelationshipsAllocations {
-  object: 'allocation';
-  attributes: {
-    id: number;
-    ip: string;
-    ip_alias: string;
-    port: number;
-    notes: string;
-    is_default: boolean;
-  };
-}
-
-interface ServerAttributeRelationships {
-  allocations: {
-    object: 'list';
-    data: ServerAttributeRelationshipsAllocations[];
-  };
-  variables: {
-    object: Record<string, unknown>;
-  };
-}
-
-interface ServerAttributes {
-  server_owner: boolean;
-  identifier: string;
-  internal_id: number;
-  uuid: string;
-  name: string;
-  node: string;
-  sftp_details: { ip: string; port: number };
-  description: string;
-  limits: ServerAttributeLimits;
-  invocation: string;
-  docker_image: string;
-  egg_features: string[];
-  feature_limits: ServerAttributeFeatureLimits;
-  status: number;
-  is_suspended: boolean;
-  is_installing: boolean;
-  is_transferring: boolean;
-  relationships: ServerAttributeRelationships;
-}
-
-interface PterodactylServer {
-  object: 'server';
-  attributes: ServerAttributes;
-}
-
-export interface ListServersResponse {
-  object: 'list';
-  data: PterodactylServer[];
-  meta: {
-    pagination: {
-      total: number;
-      count: number;
-      per_page: number;
-      current_page: number;
-      total_pages: number;
-      links: Record<string, unknown>;
-    };
-  };
 }
 
 interface PterodactylFile {
