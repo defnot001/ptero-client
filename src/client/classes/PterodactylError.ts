29c670bf8475a40b87687fd873acc6d115949afb
defnot001 <marc.miller994@gmail.com>
2022-12-15 11:10:46 +0100

    changed to throw one string only

--- src/client/classes/PterodactylError.ts
+++ src/client/classes/PterodactylError.ts
@@ -1,16 +1,12 @@
 import type { APIError } from '../types/interfaces';
 
 export class PterodactylError extends Error {
-  public readonly code: string;
-  public readonly status: string;
-  public readonly detail: string;
+  public override message: string;
 
   public constructor(error: APIError) {
     const { code, status, detail } = error;
     super(detail);
 
-    this.code = code;
-    this.status = status;
-    this.detail = detail;
+    this.message = `Status: ${status} (${code}): ${detail}`;
   }
 }
