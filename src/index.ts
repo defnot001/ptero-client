ed7bd13d16f14ef63b7aadfda335c6941eee6aad
defnot001 <marc.miller994@gmail.com>
2022-12-26 18:31:59 +0100

    added type exports for useful types

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import PteroClient from './classes/client';
 
 export default PteroClient;
 
-// export type { AuthDetails, BackupOptions, RenameOptions } from './types/types';
-// export type { AccountDetails } from './validation/AccountSchema';
-// export type {
-//   PterodactylBackup,
-//   PterodactylBackupListMeta,
-// } from './validation/BackupSchema';
-// export type { PterodactylFile } from './validation/FileSchema';
-// export type { PterodactylServer } from './validation/ServerSchema';
+export type { AuthDetails, BackupOptions, RenameOptions } from './types/types';
+export type { AccountDetails } from './validation/AccountSchema';
+export type {
+  PterodactylBackup,
+  PterodactylBackupListMeta,
+} from './validation/BackupSchema';
+export type { PterodactylFile } from './validation/FileSchema';
+export type { PterodactylServer } from './validation/ServerSchema';
