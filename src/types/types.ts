57b530fd60bd1275a34613cf8ded418eaced8d34
defnot001 <marc.miller994@gmail.com>
2023-01-08 23:03:38 +0100

    added types for Websocket

--- src/types/types.ts
+++ src/types/types.ts
@@ -33,3 +33,42 @@ export type RenameOptions = {
   to: string;
   directory?: string;
 };
+
+export type PteroServerState = 'starting' | 'running' | 'stopping' | 'offline';
+
+export type PteroStats = {
+  memory_bytes: number;
+  memory_limit_bytes: number;
+  cpu_absolute: number;
+  network: {
+    rx_bytes: number;
+    tx_bytes: number;
+  };
+  state: PteroServerState;
+  disk_bytes: number;
+};
+
+export type PteroWebsocketListeners = Readonly<{
+  onOpen?: () => void;
+  onAuthSuccess?: () => void;
+  onStatus?: (status: PteroServerState) => void;
+  onConsoleOutput?: (output: string) => void;
+  onStats?: (stats: PteroStats) => void;
+  onTokenExpiring?: () => void;
+  onTokenExpired?: () => void;
+  onClose?: (code: number, reason: Buffer) => void;
+  onError?: (error: Error) => void;
+}>;
+
+export type WebsocketResponse = {
+  event: TPteroEvent;
+  args?: string[];
+};
+
+export type TPteroEvent =
+  | 'auth success'
+  | 'status'
+  | 'console output'
+  | 'stats'
+  | 'token expiring'
+  | 'token expired';
