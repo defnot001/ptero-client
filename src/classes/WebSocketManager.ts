172f24647ebdcd6ad58d5bff12b8966af606e453
defnot001 <marc.miller994@gmail.com>
2023-01-18 23:08:10 +0100

    finished websocket implementation

--- src/classes/WebSocketManager.ts
+++ src/classes/WebSocketManager.ts
@@ -1,21 +1,25 @@
 import type { AxiosInstance } from 'axios';
-import WebSocket from 'ws';
+import { WebSocket } from 'ws';
 import type {
-  PteroServerState,
+  PowerAction,
   PteroStats,
   PteroWebsocketListeners,
+  SendObjectOptions,
   WebsocketResponse,
 } from '../types/types';
 import { ClientEndpoints, replaceVariables } from '../util/endpoints';
 import { validateResponse } from '../util/helpers';
 import {
-  ConsoleDetails,
-  WebsocketCredentialsResponse,
   WebsocketCredentialsSchema,
+  type ConsoleDetails,
+  type WebsocketCredentialsResponse,
 } from '../validation/WebsocketSchema';
 
 export default class WebsocketManager {
-  public constructor(private http: AxiosInstance) {}
+  private socket: WebSocket | null;
+  constructor(private http: AxiosInstance) {
+    this.socket = null;
+  }
 
   private async getDetails(serverID: string): Promise<ConsoleDetails> {
     const url = replaceVariables(ClientEndpoints.consoleDetails, { serverID });
@@ -27,85 +31,167 @@ export default class WebsocketManager {
     return validated.data;
   }
 
+  private sendDataToSocket(
+    socket: WebSocket | null,
+    options: SendObjectOptions,
+  ) {
+    const { objectType, action, command } = options;
+
+    if (!socket) throw new Error('Socket is not defined!');
+
+    if (objectType === 'command') {
+      socket.send(
+        JSON.stringify({
+          event: 'send command',
+          args: [command],
+        }),
+      );
+    } else if (objectType === 'state') {
+      socket.send(
+        JSON.stringify({
+          event: 'set state',
+          args: [action],
+        }),
+      );
+    } else if (objectType === 'logs' || objectType === 'stats') {
+      socket.send(
+        JSON.stringify({
+          event: `send ${objectType}`,
+          args: [null],
+        }),
+      );
+    }
+  }
+
   public async connect(
     serverID: string,
-    listeners: PteroWebsocketListeners,
-  ): Promise<WebSocket> {
+    callbackfn: (successMessage: string) => void,
+  ) {
     const details = await this.getDetails(serverID);
 
-    const ws = new WebSocket(details.socket);
+    this.socket = new WebSocket(details.socket);
+    this.socket.once('open', () => {
+      if (this.socket) {
+        this.socket.send(
+          JSON.stringify({
+            event: 'auth',
+            args: [details.token],
+          }),
+        );
+      } else {
+        throw new Error('Socket is not defined!');
+      }
+    });
 
-    const tokenObj = {
-      event: 'auth',
-      args: [details.token],
+    this.socket.on('message', async (data) => {
+      if (!this.socket) {
+        throw new Error('Socket is not defined!');
+      }
+
+      try {
+        const res = JSON.parse(data.toString()) as WebsocketResponse;
+
+        if (res.event === 'auth success') {
+          callbackfn(
+            `Authentification successful. Connected to server: ${serverID}.`,
+          );
+        }
+
+        if (res.event === 'token expiring') {
+          this.socket.send(
+            JSON.stringify(await (await this.getDetails(serverID)).token),
+          );
+        }
+      } catch (err) {
+        console.error('Failed to parse JSON!');
+      }
+    });
+  }
+
+  public on<TEvent extends keyof PteroWebsocketListeners>(
+    eventName: TEvent,
+    callback: PteroWebsocketListeners[TEvent],
+  ) {
+    if (!callback) return;
+
+    if (!this.socket) {
+      throw new Error('Socket is not defined!');
+    }
+
+    this.socket.on('message', async (data) => {
+      try {
+        const response = JSON.parse(data.toString()) as WebsocketResponse;
+
+        if (response.event === eventName) {
+          if (eventName === 'auth success') {
+            callback(undefined as any);
+          }
+
+          if (eventName === 'token expiring') {
+            callback('Token expiring.' as any);
+          }
+
+          if (eventName === 'token expired') {
+            callback('Token expired.' as any);
+          }
+
+          if (eventName === 'console output') {
+            response.args?.forEach(callback as any);
+          }
+
+          if (eventName === 'stats') {
+            if (!response.args?.[0]) return;
+            const stats = JSON.parse(response.args[0]) as PteroStats;
+            callback(stats as any);
+          }
+
+          if (eventName === 'status') {
+            callback(response.args as any);
+          }
+        }
+      } catch (err) {
+        console.error('Failed to parse JSON!');
+      }
+    });
+
+    this.socket.onerror = (err) => {
+      callback(err as any);
     };
 
-    ws.on('open', () => {
-      ws.send(JSON.stringify(tokenObj));
+    this.socket.onclose = (event) => {
+      callback(event as any);
+    };
+  }
 
-      if (listeners.onOpen) {
-        listeners.onOpen();
-      }
+  public close() {
+    if (this.socket) {
+      this.socket.close();
+    }
+  }
+
+  public sendStats() {
+    this.sendDataToSocket(this.socket, {
+      objectType: 'stats',
     });
-    ws.on('close', (code, reason) => {
-      if (listeners.onClose) {
-        listeners.onClose(code, reason);
-      }
+  }
+
+  public sendLogs() {
+    this.sendDataToSocket(this.socket, {
+      objectType: 'logs',
     });
-    ws.on('error', (error) => {
-      if (listeners.onError) {
-        listeners.onError(error);
-      }
+  }
+
+  public sendCommand(command: string) {
+    this.sendDataToSocket(this.socket, {
+      objectType: 'command',
+      command,
     });
+  }
 
-    ws.on('message', (data) => {
-      const jsonData = JSON.parse(data.toString()) as WebsocketResponse;
-
-      // server custom listeners
-      if (jsonData.event === 'stats' && jsonData.args && jsonData.args[0]) {
-        const stats = JSON.parse(jsonData.args[0]) as PteroStats;
-
-        if (listeners.onStats) {
-          listeners.onStats(stats);
-        }
-      } else if (
-        jsonData.event === 'status' &&
-        jsonData.args &&
-        jsonData.args[0]
-      ) {
-        const status = jsonData.args[0] as PteroServerState;
-
-        if (listeners.onStatus) {
-          listeners.onStatus(status);
-        }
-      } else if (jsonData.event === 'console output' && jsonData.args) {
-        if (listeners.onConsoleOutput) {
-          jsonData.args.forEach(listeners.onConsoleOutput);
-        }
-      } else if (jsonData.event === 'auth success') {
-        if (listeners.onAuthSuccess) {
-          listeners.onAuthSuccess();
-        }
-      } else if (jsonData.event === 'token expiring') {
-        if (listeners.onTokenExpiring) {
-          listeners.onTokenExpiring();
-        }
-
-        this.getDetails(serverID).then((newDetails) => {
-          const newTokenObj = {
-            event: 'auth',
-            args: [newDetails.token],
-          };
-
-          ws.send(JSON.stringify(newTokenObj));
-        });
-      } else if (jsonData.event === 'token expired') {
-        if (listeners.onTokenExpired) {
-          listeners.onTokenExpired();
-        }
-      }
+  public setState(state: PowerAction) {
+    this.sendDataToSocket(this.socket, {
+      objectType: 'state',
+      action: state,
     });
-
-    return ws;
   }
 }
